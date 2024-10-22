fc5d8fd37cd7fe966ea697beef35275f33c80cf4
krwd1117 <krwd1117@gmail.com>
2024-10-22 23:21:21 +0900

    WIL
    - record
    - Bean 이름 변경
    - 기존의 Bean을 사용하여 새로운 Bean 생성

--- BackEnd/Spring/learn-spring/src/main/java/com/krwd/learn_spring/App02HelloWorldSpring.java
+++ BackEnd/Spring/learn-spring/src/main/java/com/krwd/learn_spring/App02HelloWorldSpring.java
@@ -18,6 +18,27 @@ public class App02HelloWorldSpring {
 		// name - @Bean
 		
 		// 3. Retrieving Beans managed by Spring
-		System.out.println(context.getBean("name"));
+		String name = (String) context.getBean("name");
+		System.out.println(name);
+		
+//		int age = (int) context.getBean("age");
+		int age = (int) context.getBean("age2");
+		System.out.println(age);
+		
+		Person person = (Person) context.getBean("person");
+		System.out.println(person);
+		
+		Person person2MethodCall = (Person) context.getBean("person2MethodCall");
+		System.out.println(person2MethodCall);
+		
+		Person person3Parameters = (Person) context.getBean("person3Parameters");
+		System.out.println(person3Parameters);
+		
+		Address address = (Address) context.getBean("address2");
+		System.out.println(address);
+		
+		// Address.class 라는 타입의 빈을 얻고 싶을 때
+//		Address address2 = context.getBean(Address.class);
+//		System.out.println(address2);
 	}
 }
--- BackEnd/Spring/learn-spring/src/main/java/com/krwd/learn_spring/HelloWorldConfiguration.java
+++ BackEnd/Spring/learn-spring/src/main/java/com/krwd/learn_spring/HelloWorldConfiguration.java
@@ -3,12 +3,52 @@ package com.krwd.learn_spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {};
+record Address(String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
+	// Bean의 기본 값은 메소드의 이름을 따라감
 	@Bean
 	public String name() {
 		return "Ranga";
 	}
 	
+	// Bean의 이름을 할당할 수 있음
+	@Bean(name = "age2")
+	public int age() {
+		return 30;
+	}
+	
+	@Bean
+	public Person person() {
+		var person = new Person("Ravi", 2, new Address("Main Street", "Utrecht"));
+		return person;
+	}
+	
+	// 기존의 빈을 사용하여 새로운 빈을 생성
+	@Bean
+	public Person person2MethodCall() {
+		var person = new Person(name(), age(), address());
+		return person;
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) { // name, age, address2
+		var person = new Person(name, age, address3);
+		return person;
+	}
+	
+	@Bean(name = "address2")
+	public Address address() {
+		var address = new Address("Baker Street", "London");
+		return address;
+	}
+	
+	@Bean(name = "address3")
+	public Address address3() {
+		var address = new Address("Motinagar", "Hyderabad");
+		return address;
+	}
 }
